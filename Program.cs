231872ee9862dce34d6991777df8c261b907156d
Fabian Preuer <fabian.preuer@students.bs-linz2.ac.at>
2023-11-30 16:28:20 +0100

    add f34 and f69

--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! m1 commit f1 remote change f16");
-
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! m1 commit f1 remote change f16 f69");
