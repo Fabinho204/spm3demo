dd0be00f8b767ad210c69315ad0c33f6e7b5a545
Fabian Preuer <fabian.preuer@students.bs-linz2.ac.at>
2023-11-30 14:01:34 +0100

    update console output

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Git is cool. I bin miad. Yallahhh");
+Console.WriteLine("Hello, World! Git is cool. I bin miad. Yallahhh. OKAYYYY LETS GO ");
