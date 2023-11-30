5cb9ed682d30ee5c0b6ca085d8a7d26b600dca5f
Fabian Preuer <fabian.preuer@students.bs-linz2.ac.at>
2023-11-30 16:27:47 +0100

    add f16 - reworded

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! m1 commit f1 remote change");
+Console.WriteLine("Hello, World! m1 commit f1 remote change f16");
+
